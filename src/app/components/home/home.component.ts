90a47502840fb6cb701c13a7da75b175ffb49ed2
Austin Campbell <austincamp@gmail.com>
2021-12-15 10:47:24 -0500

    home changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,16 +8,11 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-<<<<<<< HEAD
-=======
-  // Add angular router type
->>>>>>> 33725da2ca66179f346de8c320dbf9c1fdc382fe
   constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-<<<<<<< HEAD
   // navigate to dashboard
   onLoadDashboard(){
     this.router.navigate(['/dashboard']);
@@ -26,11 +21,6 @@ export class HomeComponent implements OnInit {
   // navigate to vm-management
   onLoadVmManagement(){
     this.router.navigate(['/vm-management']);
-=======
-  // navigate to dashboard page
-  onLoadDashboard() {
-    this.router.navigate(['/dashboard']); // navigate takes route path
->>>>>>> 33725da2ca66179f346de8c320dbf9c1fdc382fe
   }
 
 }
